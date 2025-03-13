82811cabd8178665feb93a8babe898bb9a7e0ef8
James Ross <git@james-ross.co.uk>
2025-03-13 00:05:50 +0000

    fix: Add single digit hour date/time format support and skip 12-hour formats entirely

--- JGR.DateTime/JGR.DateTime.psm1
+++ JGR.DateTime/JGR.DateTime.psm1
@@ -12,7 +12,16 @@ function Format-DateTime {
 
     $rollover = $env:JGR_DAY_ROLLOVER ?? 0
     Write-Debug ('Format-DateTime: DateTime={0} Format={1} Rollover={2}' -f $DateTime, $Format, $rollover)
-    $hour12 = $DateTime.ToString('hh')
-    $hour24 = '{0:D2}' -f ($(if ($DateTime.Hour -lt $rollover) { $DateTime.Hour + 24 } else { $DateTime.Hour }))
-    $DateTime.AddHours(-$rollover).ToString(($Format -creplace 'hh', '''[hh]''' -creplace 'HH', '''[HH]''')) -creplace '\[hh\]', $hour12 -creplace '\[HH\]', $hour24
+    if ($Format -cmatch 'hh?') {
+        Write-Warning ('Format-DateTime: Skipping rollover for format with 12-hour time component: {0}' -f $Format)
+        $DateTime.ToString($Format)
+    }
+    else {
+        $hour24_1 = '{0:D1}' -f ($DateTime.Hour -lt $rollover ? $DateTime.Hour + 24 : $DateTime.Hour)
+        $hour24_2 = '{0:D2}' -f ($DateTime.Hour -lt $rollover ? $DateTime.Hour + 24 : $DateTime.Hour)
+        $formatInput = $Format -creplace 'HH?', '''[$0]'''
+        $formatOutput = $DateTime.AddHours(-$rollover).ToString($formatInput)
+        Write-Debug ('Format-DateTime: FormatInput={0} FormatOutput={1}' -f $formatInput, $formatOutput)
+        $formatOutput -creplace '\[H\]', $hour24_1 -creplace '\[HH\]', $hour24_2
+    }
 }
